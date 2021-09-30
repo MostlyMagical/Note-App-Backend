32545d581cfa22fe7552a5d9b3693f9bb9894484
MostlyMagical <jb.mccann@outlook.com>
2021-09-30 18:50:13 +0100

    populated routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,26 @@
 const {Router} = require("express")
 const helloRouter = Router()
 const {
-
+    addUser, 
+    updateUser,
+    deleteUser,
+    login,
+    tokenLogin,
+    addNote,
+    updateNote,
+    deleteNote
 } = require("../controllers")
 
 const {
-
+    hashPassword,
+    createToken,
+    decodeToken
 } = require("../middleware")
 
+helloRouter.post("/user", hashPassword, createToken, addUser, addNote)
+helloRouter.patch("/user", hashPassword, updateUser, updateNote)
+helloRouter.delete("/user/:username", deleteUser, deleteNote)
+helloRouter.post("/user/login", createToken, login)
+helloRouter.get("/user", decodeToken, tokenLogin)
+
+module.exports = helloRouter
